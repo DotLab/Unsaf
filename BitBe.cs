2b17a5fee3f23a0341de432f77f60301e4a4a178
Kailang <kailangfu@gmail.com>
2019-02-23 21:04:03 -0600

    Add ReadInt24

--- BitBe.cs
+++ BitBe.cs
@@ -1,14 +1,21 @@
 ﻿namespace Unsaf {
 	[Unity.IL2CPP.CompilerServices.Il2CppSetOption(Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]
 	public static unsafe class BitBe {
-		public static short ReadShort(byte *bytes, int *i) {
+		public static short ReadInt16(byte *bytes, int *i) {
 			int j = *i;
 			short v = (short)((sbyte)bytes[j] << 8 | bytes[j + 1]); 
 			*i = j + 2;
 			return v;
 		}
 
-		public static int ReadInt(byte *bytes, int *i) {
+		public static int ReadInt24(byte *bytes, int *i) {
+			int j = *i;
+			int v = (int)((sbyte)bytes[j] << 16 | bytes[j + 1] << 8 | bytes[j + 2]); 
+			*i = j + 3;
+			return v;
+		}
+
+		public static int ReadInt32(byte *bytes, int *i) {
 			int j = *i;
 			int v = (int)((sbyte)bytes[j] << 24 | bytes[j + 1] << 16 | bytes[j + 2] << 8 | bytes[j + 3]); 
 			*i = j + 4;
